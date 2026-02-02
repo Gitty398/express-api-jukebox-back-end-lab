0e3432f564d58d251af37ce950d34aca89e6db6f
John F <anabasisinc@outlook.com>
2026-02-02 17:36:08 -0500

    added route for get track

--- controllers/track.controller.js
+++ controllers/track.controller.js
@@ -3,7 +3,19 @@ const Track = require("../models/track")
 const express = require("express")
 const router = express.Router()
 
+// GET /tracks/:trackId
 
+router.get("/:trackId", async (req, res) => {
+    try {
+        const foundTrack = await Track.findById(req.params.trackId);
+
+        if (!foundTrack) return res.status(404).json({ message: "Track Not Found" });
+
+        res.status(200).json({ foundTrack });
+    } catch (error) {
+        res.status(500).json({ err: error.message });
+    }
+});
 
 
 
