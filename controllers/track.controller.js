734c10aee422a5a2c03525f563846b2c6ffa9b59
Thomas Phillips <fatesunited@gmail.com>
2026-02-02 16:34:46 -0600

    finished get all tracks

--- controllers/track.controller.js
+++ controllers/track.controller.js
@@ -4,7 +4,14 @@ const express = require("express")
 const router = express.Router()
 
 
-
-
+// GET /tracks
+router.get("/", async (req, res) => {
+  try {
+    const tracks = await Track.find();
+    res.status(200).json(tracks);
+  } catch (error) {
+    res.status(500).json({ err: error.message });
+  }
+});
 
 module.exports = router;
